b10081628caa4d4d3c6ad27e9fa5172916cd75b0
Paul Hidalgo <paul.hidalgo@bayteq.com>
2018-07-20 12:17:09 -0500

    Scripts for validation of version database

--- app/src/main/java/com/bhp/securitytest/db/UserDatabase.kt
+++ app/src/main/java/com/bhp/securitytest/db/UserDatabase.kt
@@ -9,8 +9,7 @@ import android.arch.persistence.room.migration.Migration
 import android.content.Context
 import com.bhp.securitytest.Converters
 
-//TODO change version database
-@Database(entities = arrayOf(User::class, Register::class, UserRegister::class), version = 2)
+@Database(entities = arrayOf(User::class, Register::class, UserRegister::class), version = 3)
 @TypeConverters(Converters::class)
 abstract class UserDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
@@ -26,7 +25,7 @@ abstract class UserDatabase : RoomDatabase() {
                 synchronized(UserDatabase::class) {
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
                             UserDatabase::class.java, "weather.db")
-                            .addMigrations(MIGRATION_1_2, MIGRATION_2_3 )
+                            .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                             .build()
                 }
             }
@@ -46,6 +45,26 @@ abstract class UserDatabase : RoomDatabase() {
 
     object MIGRATION_2_3 : Migration(2, 3) {
         override fun migrate(database: SupportSQLiteDatabase) {
+            //Create new table
+            database.execSQL("CREATE TABLE users_new (id TEXT NOT NULL, idType TEXT NOT NULL DEFAULT 'C', name TEXT NOT NULL, lastname TEXT NOT NULL, email TEXT NOT NULL, company TEXT NOT NULL, position TEXT NOT NULL, q1 INTEGER, q2 INTEGER, date INTEGER NOT NULL, success INTEGER, PRIMARY KEY(id))")
+
+            //Copy data
+            database.execSQL("INSERT INTO users_new (id, idType, name, lastname, email, company, position, q1, q2, date, success) " +
+                    "SELECT id, idType, name, lastname, email, company, position, q1, q2, date, success FROM user")
+
+            // Remove the old table
+            database.execSQL("DROP TABLE user")
+
+            // Change the table name to the correct one
+            database.execSQL("ALTER TABLE users_new RENAME TO user")
+
+            //Create new table register
+            database.execSQL("CREATE TABLE register (id INTEGER NOT NULL, state TEXT NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))")
+
+            //Create new table user_register
+            database.execSQL("CREATE TABLE user_register (id INTEGER NOT NULL, user_id TEXT NOT NULL, register_id INTEGER NOT NULL, date INTEGER NOT NULL, hour INTEGER NOT NULL, PRIMARY KEY(id), " +
+                    "FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE NO ACTION ON UPDATE NO ACTION, " +
+                    "FOREIGN KEY(register_id) REFERENCES register(id) ON DELETE NO ACTION ON UPDATE NO ACTION)")
 
         }
     }
