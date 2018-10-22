15e034b4c6a0d426a2e50c283ba0203cc6735198
Paul Hidalgo <paul.alex.hidalgo@gmail.com>
2018-10-22 12:05:39 -0500

    - Cambio de idioma 100%

--- app/src/main/java/com/bhp/securitytest/BaseActivity.kt
+++ app/src/main/java/com/bhp/securitytest/BaseActivity.kt
@@ -3,11 +3,13 @@ package com.bhp.securitytest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.bhp.securitytest.helpers.LocaleHelper
 
 abstract class BaseActivity : NotificationBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        LocaleHelper.setLocale(this@BaseActivity, LocaleHelper.getLanguage(this@BaseActivity))
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
